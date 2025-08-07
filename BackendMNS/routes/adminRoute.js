3dc5efd8eb52a2945717c4c68a8c291630f89fed
Bikash-design-lab <vmodi5425@gmail.com>
2025-08-07 12:49:06 +0530

    updated experiance to array of experiance

--- BackendMNS/routes/adminRoute.js
+++ BackendMNS/routes/adminRoute.js
@@ -7,7 +7,7 @@ import { loginUser, logoutUser, registerSuperAdmin, registerUser } from '../cont
 import User from '../models/User.js';
 import { deleteUserByID, getAllUsers, getUnverifiedUsers, getUserDetailsByID, userStatusUpdate, verifyUser } from '../controllers/superadminController.js';
 import Student from '../models/Student.js';
-
+import Teacher from '../models/Teacher.js'
 const adminRouter = express.Router();
 
 
@@ -97,9 +97,20 @@ adminRouter.post('/students/batch', protect, authorizeRoles('admin', 'superadmin
         });
 
     } catch (err) {
-        console.error(err);
+        console.error("error while granting batch of student", err);
         res.status(500).json({ message: 'Something went wrong', err });
     }
 })
+
+// adminRouter.post('/teachers/batch', protect, authorizeRoles('admin', 'superadmin'), async (req, res) => {
+//     try {
+//         // name, email, password,  contact number, address(street, city , state, zip-code), professional-summary(employee-id, deignation, subject-specialization, qualification, Date_of_joining), Class Teacher Assign(class, section), experiance(years, details), Personal information(date of birth, gender), Emergency contact(emergency contact name, emergency contact number, emergency contact relation)
+//         // create a batch of teachers
+
+//     } catch (error) {
+//         console.log("error while creating batch of teacher", error)
+//         return res.status(500).json({ message: "Error while creating a batch of student", error })
+//     }
+// })
 
 export default adminRouter;
